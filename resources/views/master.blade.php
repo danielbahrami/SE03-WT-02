f7ef49dff7f4817b42b0cda4f609097102b12dc2
Daniel Bahrami <daniel@bahrami.dk>
2021-11-10 16:30:50 +0100

    Name change

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,7 +13,7 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">
             <img src="{{ asset('imgs/paw.svg') }}" alt="" class="me-2" style="height: 24px"/>
-            <span>{Name's} Pet Shelter</span>
+            <span>Daniel & Ashvikan's Pet Shelter</span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
